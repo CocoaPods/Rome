d29063b57935645793b39c8a2a62d302bdaf0370
Samuel E. Giddins <segiddins@segiddins.me>
2015-07-18 13:36:50 -0700

    Actually require the pre-install hook

--- lib/cocoapods-rome/pre_install.rb
+++ lib/cocoapods-rome/pre_install.rb
@@ -1,4 +1,4 @@
 Pod::HooksManager.register('cocoapods-rome', :pre_install) do |installer_context|
   installer_context.podfile.use_frameworks!
-  Pod::Config.integrate_targets = false
+  Pod::Config.instance.integrate_targets = false
 end
