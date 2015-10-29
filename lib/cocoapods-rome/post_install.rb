4aa166c6bb7b041512b91166f2fedddd60102c76
Maciej Banasiewicz <mbanasiewicz@gmail.com>
2015-10-29 22:15:21 +0100

    Add naive tvos support

--- lib/cocoapods-rome/post_install.rb
+++ lib/cocoapods-rome/post_install.rb
@@ -2,6 +2,9 @@ CONFIGURATION = "Release"
 DEVICE = "iphoneos"
 SIMULATOR = "iphonesimulator"
 
+TV_DEVICE = "appletvos"
+TV_SIMULATOR = "appletvsimulator"
+
 def xcodebuild(sandbox, target, sdk='macosx')
   Pod::Executable.execute_command 'xcodebuild', %W(-project #{sandbox.project_path.basename} -scheme #{target} -configuration #{CONFIGURATION} -sdk #{sdk}), true
 end
@@ -30,6 +33,27 @@ Pod::HooksManager.register('cocoapods-rome', :post_install) do |installer_contex
           device_lib = "#{build_dir}/#{CONFIGURATION}-#{DEVICE}/#{target_label}/#{root_name}.framework/#{root_name}"
           device_framework_lib = File.dirname(device_lib)
           simulator_lib = "#{build_dir}/#{CONFIGURATION}-#{SIMULATOR}/#{target_label}/#{root_name}.framework/#{root_name}"
+
+          next unless File.file?(device_lib) && File.file?(simulator_lib)
+
+          lipo_log = `lipo -create -output #{executable_path} #{device_lib} #{simulator_lib}`
+          puts lipo_log unless File.exist?(executable_path)
+
+          FileUtils.mv executable_path, device_lib
+          FileUtils.mv device_framework_lib, build_dir
+          FileUtils.rm simulator_lib if File.file?(simulator_lib)
+          FileUtils.rm device_lib if File.file?(device_lib)
+        end
+    elsif target.platform_name == :tvos
+        xcodebuild(sandbox, target_label, TV_DEVICE)
+        xcodebuild(sandbox, target_label, TV_SIMULATOR)
+
+        spec_names = target.specs.map { |spec| spec.root.module_name }.uniq
+        spec_names.each do |root_name|
+          executable_path = "#{build_dir}/#{root_name}"
+          device_lib = "#{build_dir}/#{CONFIGURATION}-#{TV_DEVICE}/#{target_label}/#{root_name}.framework/#{root_name}"
+          device_framework_lib = File.dirname(device_lib)
+          simulator_lib = "#{build_dir}/#{CONFIGURATION}-#{TV_SIMULATOR}/#{target_label}/#{root_name}.framework/#{root_name}"
 
           next unless File.file?(device_lib) && File.file?(simulator_lib)
 
